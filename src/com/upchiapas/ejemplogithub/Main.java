7533bdaad690be8283cfb54973b451c9615342e5
diego <diegoMP123@outlook.com>
2022-05-16 13:56:09 -0600

    Actualizacion de Main

--- src/com/upchiapas/ejemplogithub/Main.java
+++ src/com/upchiapas/ejemplogithub/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
+
+        System.out.println("Bienvenido a la clase de POO");
     }
 }
